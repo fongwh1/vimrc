a06dec13c6fb9e5a5efa8d2f48caf0e831fdc0f1
willfong <willfong@synology.com>
2017-07-18 13:11:09 +0800

    add plugin: vim-javascript

--- update_plugins.py
+++ update_plugins.py
@@ -52,6 +52,7 @@ vim-pug https://github.com/digitaltoad/vim-pug
 vim-yankstack https://github.com/maxbrunsfeld/vim-yankstack
 lightline.vim https://github.com/itchyny/lightline.vim
 tagbar https://github.com/majutsushi/tagbar
+vim-javascript https://github.com/pangloss/vim-javascript
 """.strip()
 
 GITHUB_ZIP = '%s/archive/master.zip'
